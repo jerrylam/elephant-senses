ca82017b1813330687e65756d7a16009cc27dbc1
jerrylam <chilinglam@gmail.com>
2013-03-31 20:23:21 -0400

    text stream

---
+++ src/main/java/datasenses/mapreduce/io/TextStream.java
@@ -0,0 +1,50 @@
+package datasenses.mapreduce.io;
+
+import java.io.BufferedReader;
+import java.io.DataInput;
+import java.io.DataInputStream;
+import java.io.DataOutput;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
+import java.nio.charset.CodingErrorAction;
+
+import javax.activation.UnsupportedDataTypeException;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.hadoop.io.MapFile;
+import org.apache.hadoop.io.MapFile.Reader;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.WritableUtils;
+
+public class TextStream extends Text {
+
+	private final DataOutputStream out;
+
+	private static ThreadLocal<CharsetDecoder> DECODER_FACTORY = new ThreadLocal<CharsetDecoder>() {
+		protected CharsetDecoder initialValue() {
+			return Charset.forName("UTF-8").newDecoder()
+					.onMalformedInput(CodingErrorAction.REPORT)
+					.onUnmappableCharacter(CodingErrorAction.REPORT);
+		}
+	};
+
+	public TextStream(DataOutputStream out) {
+		this.out = out;
+	}
+
+	@Override
+	public void readFields(DataInput in) throws IOException {
+		// streamLength = WritableUtils.readVInt(in);
+		in.readLine();
+		CharsetDecoder decoder = DECODER_FACTORY.get();
+		if (in instanceof DataInputStream)
+			IOUtils.copy(new BufferedReader(new InputStreamReader((DataInputStream) in, decoder)), out);
+		else {
+			throw new UnsupportedDataTypeException();
+		}
+	}
+}
